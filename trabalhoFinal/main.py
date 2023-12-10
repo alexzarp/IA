3b647434547ffd123bdc5c0148f444f2d8cdaea7
alexzarp <alexszds@gmail.com>
2023-12-10 17:31:35 -0300

    up

--- trabalhoFinal/main.py
+++ trabalhoFinal/main.py
@@ -3,20 +3,46 @@ import os
 from datetime import datetime
 import numpy as np
 from multiprocessing import Process
+import csv
 
 
-def obter_informacoes_imagem(imagem):
+def salvar_resultados_csv(nome_arquivo, k, info_original, info_gerada):
+    with open(nome_arquivo.replace(".png", ".csv"), "a", newline="") as csvfile:
+        colunas = [
+            "K",
+            "Tamanho KB Original",
+            "Tamanho KB Gerado",
+            "Cores unicas originais",
+            "Cores unicas geradas",
+        ]
+        writer = csv.DictWriter(csvfile, fieldnames=colunas)
+
+        # Se o arquivo não existir, escreva os cabeçalhos
+        if csvfile.tell() == 0:
+            writer.writeheader()
+
+        # Escreve a linha de dados
+        writer.writerow(
+            {
+                "K": k,
+                "Tamanho KB Original": info_original[1],
+                "Tamanho KB Gerado": info_gerada[1],
+                "Cores unicas originais": info_original[2],
+                "Cores unicas geradas": info_gerada[2],
+            }
+        )
+
+
+def obter_informacoes_imagem(imagem, imagem_path):
     # Obtém resolução, tamanho em KB e quantidade de cores únicas
     resolucao = imagem.shape[:2]
-    # Obtemos diretamente o tamanho da imagem em bytes usando a função imread
-    tamanho_bytes = cv2.imencode(".jpg", imagem)[1].tobytes()
-    # Convertendo para kilobytes
-    tamanho_kb = len(tamanho_bytes) / 1024.0
+    # Obtém o tamanho real dos dados codificados
+    tamanho_kb = os.stat(imagem_path).st_size / 1024.0
     cores_unicas = len(set(tuple(pixel) for linha in imagem for pixel in linha))
     return resolucao, tamanho_kb, cores_unicas
 
 
-def aplicar_kmeans_e_salvar(imagem, k_values, pasta_saida, nome_original):
+def aplicar_kmeans_e_salvar(imagem, k_values, pasta_saida, nome_original, imagem_path):
     def processar(k):
         # Aplicar o algoritmo K-Means
         pixels = imagem.reshape((-1, 3))
@@ -34,8 +60,7 @@ def aplicar_kmeans_e_salvar(imagem, k_values, pasta_saida, nome_original):
         imagem_segmentada = centroides[kmeans[1].flatten()].reshape(imagem.shape)
 
         # Obter informações sobre as imagens original e gerada
-        info_original = obter_informacoes_imagem(imagem)
-        info_gerada = obter_informacoes_imagem(imagem_segmentada)
+        info_original = obter_informacoes_imagem(imagem, imagem_path)
 
         # Salvar imagens
         nome_arquivo = f'resultado_k{k}_{"x".join(map(str, info_original[0]))}.jpg'
@@ -44,8 +69,16 @@ def aplicar_kmeans_e_salvar(imagem, k_values, pasta_saida, nome_original):
             cv2.cvtColor(imagem_segmentada, cv2.COLOR_RGB2BGR),
         )
 
+        # Obter informações sobre as imagens original e gerada
+        info_gerada = obter_informacoes_imagem(
+            imagem_segmentada, pasta_saida + "/" + nome_arquivo
+        )
+
         # Imprimir informações
         print(f"{nome_original}: K = {k}: {info_original} -> {info_gerada}")
+        salvar_resultados_csv(
+            pasta_saida + nome_original[1:], k, info_original, info_gerada
+        )
 
     process = []
     for k in k_values:
@@ -96,7 +129,7 @@ if __name__ == "__main__":
         # Aplicar K-Means e salvar imagens
         proc = Process(
             target=aplicar_kmeans_e_salvar,
-            args=(imagem_original, valores_k, pasta_local, nome_original),
+            args=(imagem_original, valores_k, pasta_local, nome_original, imagem_path),
         )
         procs.append(proc)
         proc.start()
